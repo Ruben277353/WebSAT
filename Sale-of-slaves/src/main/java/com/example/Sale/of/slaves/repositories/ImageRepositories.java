e5c87640fbab0fdd64295966f7c5c506f284744e
Naumov AL <144628355+Ruben277353@users.noreply.github.com>
2024-06-06 15:26:28 +0300

    12
    12

--- Sale-of-slaves/src/main/java/com/example/Sale/of/slaves/repositories/ImageRepositories.java
+++
@@ -1,8 +0,0 @@
-package com.example.Sale.of.slaves.repositories;
-
-import com.example.Sale.of.slaves.models.Image;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ImageRepositories extends JpaRepository<Image,Long> {
-
-}
--- Sale-of-slaves/src/main/java/com/example/Sale/of/slaves/repositories/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.Sale.of.slaves.repositories;
-
-import com.example.Sale.of.slaves.models.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User,Long> {
-    User findByEmail(String email);
-}
